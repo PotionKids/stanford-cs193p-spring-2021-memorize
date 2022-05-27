2b8cb18a85836f5b76688fa7bb86638a7c9dce55
kris-randen <kris.randen@gmail.com>
2022-05-27 15:57:51 +0530

    Added 4 Cards

--- memorize/memorize/ContentView.swift
+++ memorize/memorize/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0..<4) {_ in
+                CardView()
+            }
+        }
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Hello Scoop!")
+        }
+        .foregroundColor(.red)
     }
 }
 

43eb82de7abf00a84b387d67952355d00202132d
kris-randen <kris.randen@gmail.com>
2022-05-28 18:05:52 +0530

    Completed matching logic of the game and made matching cards disappear.

--- memorize/memorize/Model/MemoryGame.swift
+++ memorize/memorize/Model/MemoryGame.swift
@@ -7,13 +7,37 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent: Equatable> {
     private(set) var cards: [Card]
+    private var onlyFaceUpCard: Int?
     
     mutating func choose(_ card: Card) {
-        guard let chosenIndex = cards.firstIndex(where: {$0.id == card.id}) else { return }
+        print("indexOfTheOnlyFaceUpCard = \(String(describing: onlyFaceUpCard ?? nil))")
+        guard let chosenIndex = cards.firstIndex(where: {$0.id == card.id}),
+                cards[chosenIndex].isNotFaceUp,
+                cards[chosenIndex].isNotMatched
+        else { return }
+        match(chosenIndex)
         cards[chosenIndex].isFaceUp.toggle()
     }
+    
+    mutating func turnAllCardsFaceDown() {
+        for index in cards.indices {
+            cards[index].isFaceUp = false
+        }
+    }
+    
+    mutating func match(_ chosenIndex: Int) {
+        guard let potentialMatch = onlyFaceUpCard else {
+            turnAllCardsFaceDown()
+            onlyFaceUpCard = chosenIndex
+            return
+        }
+        cards[chosenIndex].isMatched = true
+        cards[potentialMatch].isMatched = true
+        onlyFaceUpCard = nil
+    }
+    
     
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
         cards = [Card]()
@@ -26,9 +50,16 @@ struct MemoryGame<CardContent> {
     }
     
     struct Card: Identifiable {
-        var isFaceUp = true
+        var isFaceUp = false
         var isMatched = false
         var content: CardContent
         var id: Int
+        
+        var isNotFaceUp: Bool {
+            !isFaceUp
+        }
+        var isNotMatched: Bool {
+            !isMatched
+        }
     }
 }
--- memorize/memorize/Views/CardView.swift
+++ memorize/memorize/Views/CardView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct CardView: View {
-    let card: MemoryGame<String>.Card
+    let card: Game.Card
     
     var body: some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
-            shape.fill().foregroundColor(card.isFaceUp ? .white : .red)
-            shape.strokeBorder(lineWidth: 2)
-            Text(card.isFaceUp ? card.content : "").font(.largeTitle)
+            if card.isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 2)
+                Text(card.content).font(.largeTitle)
+            }
+            else if card.isMatched { shape.opacity(0) }
+            else { shape.fill() }
         }
     }
 }
